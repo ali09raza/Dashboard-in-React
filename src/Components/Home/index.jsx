0678e7a4f00a5a2792844b17a17700e6e29fc993
Ali Raza <aliraza01369@gmail.com>
2023-12-16 16:15:51 +0500

    done

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,60 +1,17 @@
 import React from "react";
-import './home.scss'
+import './home.scss';
+import CustomBarChart from "../CustomBarChart";
+import CustomLineChart from "../CustomLineChart";
+import Data from "../Data";
 
 import {BsFillArchiveFill,
     BsFillGrid3X3GapFill,
     BsPeopleFill,
     BsFillBellFill
   } from 'react-icons/bs';
-  import {
-    BarChart,
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    Legend,
-    Bar,
-    LineChart,
-    Line
-  } from 'recharts';
+  
 const Home=()=>{
-    const data = [
-        {
-          "name": "Page A",
-          "uv": 4000,
-          "pv": 2400
-        },
-        {
-          "name": "Page B",
-          "uv": 3000,
-          "pv": 1398
-        },
-        {
-          "name": "Page C",
-          "uv": 2000,
-          "pv": 9800
-        },
-        {
-          "name": "Page D",
-          "uv": 2780,
-          "pv": 3908
-        },
-        {
-          "name": "Page E",
-          "uv": 1890,
-          "pv": 4800
-        },
-        {
-          "name": "Page F",
-          "uv": 2390,
-          "pv": 3800
-        },
-        {
-          "name": "Page G",
-          "uv": 3490,
-          "pv": 4300
-        }
-      ]
+   
 
 
     return(
@@ -100,25 +57,10 @@ const Home=()=>{
 
             </div>
             <div className="charts">
-        <BarChart width={730} height={250} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-        <LineChart width={730} height={250} data={data}
-  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="name" />
-  <YAxis />
-  <Tooltip />
-  <Legend />
-  <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-  <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-</LineChart>
+              <CustomBarChart data={Data}/>
+              <CustomLineChart data={Data}/>
+       
+     
       </div>
 
         </div> 
