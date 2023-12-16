63913c1f73f49e740f6c73b7b7d7d5dfaa15a8a9
Ali Raza <aliraza01369@gmail.com>
2023-12-16 15:51:15 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import Header from './Components/Header'
+
 import Home from './Components/Home'
 import SideBar from './Components/SideBar'
 
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="grid-container">
-      <Header/>
+      
       <SideBar/>
       <Home/>
 
