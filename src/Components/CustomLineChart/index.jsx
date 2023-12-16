fbb2b5f7496384b47bff747587338a86ea3dc897
Ali Raza <aliraza01369@gmail.com>
2023-12-16 16:16:14 +0500

    done1

---
+++ src/Components/CustomLineChart/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+ 
+    LineChart,
+    Line
+  } from 'recharts';
+
+const CustomLineChart = ({ data }) => {
+  return (
+    <LineChart width={730} height={250} data={data}
+    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+  </LineChart>
+  );
+};
+
+export default CustomLineChart;
